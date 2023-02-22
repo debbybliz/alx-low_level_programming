59631379138cf653a9096fe266a4240ad4944dca
Deborah Falope <debibliz1965@gmail.com>
2023-02-21 16:04:05 -0800

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,23 +1,4 @@
-#include "main.h"
-
-/**
- * main - computes and prints the sum of all the multiples of 3 0r 5 below 1024
- * Return: 0 if successful
- */
-
-int main(void)
-{
-	int sum, i;
-
-	for (i = 1023, sum = 0; i; i--)
-		if (!(i % 3) || !(i % 5))
-		sum += i;
-printf("%d\n", sum);
-
-return (0);
-}
-[11:47 PM, 2/21/2023] 🎼Shooga👨‍💻 ALX: 13
-[11:48 PM, 2/21/2023] 🎼Shooga👨‍💻 ALX: #include <stdio.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
