329cc0a584ec84c5a47c75beba75c40113a0de87
Deborah Falope <debibliz1965@gmail.com>
2023-04-08 14:24:40 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,36 +1,31 @@
 #include "main.h"
-#include <stdlib.h>
- /**
-  * read a textfile and print it to POSIX stdout.
-  * @letter:The number of letters the function should print and read.
-  * @filename:A pointer to the name of the files.
-  *
-  * Return:If th function fails or is the filename is NULL -0.
-  * O/W the accurate number of bytes the function can read and print.
-  */
-ssize_t read_textfile(const char *filename, size_t letters)
+/**
+ * read_textfile - read a text file and print it to the posix standard
+ * @filename: pointer to text in a file
+ * @letters: number of letters
+ * Return: the actual numer of letters it could read and print
+ */
+ssize_t read_textfile(cont char *filename, size_t letters)
 {
-	ssize_t o, r, w;
-	char *buffer;
+	ssize_t file, fread fwrite;
+	char *totalSize;
 
+	totalSize = malloc(sizeof(char) * letters);
+	if (totalSize == NULL)
+		return (0);
 	if (filename == NULL)
 		return (0);
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	file = open(filename, O_RDONLY);
+	if (file == 1)
 		return (0);
-
-	o = open(filename, O_RDONLY);
-	r = read(o, buffer, letters);
-	w = write(STDOUT_FILENO, buffer, r);
-
-	if (o == -1 || r == -1 || w == -1 || w != r)
-{
-	free(buffer);
-	return (0);
-}
-        free(buffer);
-	close(o);
-
-	return (w);
+	fread = read(file, totalSize, letters);
+	if (fread == -1)
+		return (0);
+	fwrite = write(STDOUT_FILENO, totalSize, fread);
+	if (fwrite == -1)
+		return (0);
+	close(file);
+	free(totalSize);
+	return (fwrite);
 }
